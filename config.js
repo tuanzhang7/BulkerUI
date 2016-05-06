e69bcb9a0217cb536186333e2f9eb4a920175a73
tuanzhang <tuanzhang@gmail.com>
2016-05-06 16:18:51 +0800

    add query by name or nodeId from url parameter

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   "mongodb": {
     "host": "172.30.11.195",//localhost
     "port":"3306",//27017
-    "db":"cms"
+    "db":"ncms"
   },
   "setting":{
     "collection":"workspace"
--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,26 @@ var db = config.mongodb.db;
 var url = 'mongodb://' + host + ':' + port + '/' + db;
 var coll=config.setting.collection;
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:keyword?', function(req, res, next) {
+    var keyword = req.params.keyword;
+
     MongoClient.connect(url, function (err, db) {
         var collection = db.collection(coll);
         var query = {};
+        if (keyword) {
+            var intKeyword=parseInt(keyword);
+            query = {$or: [{"cm:name": keyword}, {id: intKeyword}]};
+        }
+
         var projection = {_id: 0};
         var sort = {id: 1};
         collection.find(query, projection).sort(sort).limit(2).toArray(function (err, docs) {
-            //console.log(docs);
-            //var retulst = JSON.stringify(docs);
-            db.close();
-            res.render('index', {"title":"Bulker UI","docs":docs});
+            collection.find(query).count(function (err, count) {
+                //console.log(docs);
+                //var retulst = JSON.stringify(docs);
+                db.close();
+                res.render('index', {"title":"Bulker UI","docs":docs,"count":count});
+            });
         });
     });
     // res.render('index', {});
